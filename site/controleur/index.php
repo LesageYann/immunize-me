4bda01385e8175cdef8067d64c57ed22fcb6f269
Cyril Ferlicot <cyril.ferlicot@gmail.com>
2014-12-05 02:23:57 +0100

    site amelioration
    update de l'index.

--- site/controleur/index.php
+++ site/controleur/index.php
@@ -26,9 +26,22 @@
 	<div id="principal">
 		<div id="marge">
 			
-		<h3> Un petit jeu ? </h3>
-	
+			<h3 id="titrejeu"> Un petit jeu ? </h3>
+			<div class="sousdiv" id="jeu">
+			
+			</div>
 		</div>
+		<h3> Description </h3>
+			<div class="sousdiv">
+				<h2> Description générale: </h2>
+				<p>
+					Bonjour ! </br>
+					Ce petit jeu que vous avez (normalement) sous les yeux est une petite application développée dans un but préventif et informatif. Il vise les plus jeunes particulièrement et a pour objectif de leur apprendre quelques informations sur des virus qui frappes notre monde aujourd'hui et sur les moyens de prévention/de soin.</br>
+					Pour ce faire ce petit jeu propose aux enfant d'aller combattre quelques virus emblématiques tel que Ebola ou le VIH tout en récoltant des informations pour les aider à comprendre de quoi ils ont besoin pour les combattres !</br>
+				</p>
+				<h2> Controles : </h2>
+				<p> Les controles sont très simples : tout se fait à la souris. Il suffit de maintenir le clic gauche de souris pour envoyer vos attaques et bouger votre arme.</p>
+			</div>
 	</div>
 	<?php 
 		include'../vue/footer.html';
